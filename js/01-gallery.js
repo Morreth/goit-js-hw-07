f8bbcd0dde8b768dad3339d9ee7599e0c994cd6c
Morreth <47885094+Morreth@users.noreply.github.com>
2022-11-17 20:14:54 +0200

    jsfiles

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+console.log(galleryItems);
 
+const galleryMain = document.querySelector(".gallery");
+const imagesCreated = createGalleryItems(galleryItems);
+galleryMain.insertAdjacentHTML("beforeend", imagesCreated);
+galleryMain.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source} "width = 1200"">
+`);
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  instance.show();
+}
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<a class="gallery__link" href="large-image.jpg">
+   <img
+     class="gallery__image"
+     src="${preview}"
+     data-source="${original}"
+     alt="${description}"
+   />
+ </a>`
+    )
+    .join("");
+}
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
+const galleryMain = document.querySelector(".gallery");
+const imagesCreated = createGalleryItems(galleryItems);
+galleryMain.insertAdjacentHTML("beforeend", imagesCreated);
+galleryMain.addEventListener("click", onClick);
+
+function onClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== "IMG") {
+    return;
+  }
+  lightbox.open();
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionsDelay: 250,
+});
+
+function createGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ description, preview, original }) =>
+        `<a class="gallery__link" href="${original}">
+   <img
+     class="gallery__image"
+     src="${preview}"
+     alt="${description}"
+   />
+ </a>`
+    )
+    .join("");
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
 console.log(galleryItems);
